4587eabeb842a3915283cf47322f6db774d18958
Ian <ianmcm.89@gmail.com>
2021-09-26 11:46:31 -0400

    commit tree e7197919e7866ec0a3ff2a24ab33e25d9df32264

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
-// Add your Circle class here
+class Circle{
+    constructor(radius){
+        this.radius = radius;
+    }
+
+    get diameter(){
+        return this.radius * 2;
+    }
+
+    get circumference(){
+        return this.diameter * Math.PI;
+    }
+
+    get area(){
+        return (this.radius * this.radius) * Math.PI;
+    }
+
+    set diameter(newDiameter){
+      this.radius = newDiameter / 2;
+    }
+
+    set circumference(newCircumference){
+      this.diameter = newCircumference / Math.PI;
+    }
+
+    set area(newArea){
+        this.radius = Math.sqrt(newArea / Math.PI);
+    }
+}
+
+let circle = new Circle(6);
+
+console.log('Radius = ' + circle.radius); //=> 6
+console.log('Diameter = ' + circle.diameter); //=> 12
+console.log('Circumference = ' + circle.circumference); //=> 37.68 <-> 37.71
+console.log('Area = ' + circle.area); //=> 113 <-> 113.1
+console.log('');
+
+console.log(circle.diameter = 36);
+console.log(circle.radius); //=> 18
+console.log('');
+
+console.log(circle.circumference = 100);
+console.log(circle.radius); //=> 15 <-> 16;
+console.log('');
+
+console.log(circle.area = 200);
+console.log(circle.radius); //=> 7 <-> 8;
+console.log('');
